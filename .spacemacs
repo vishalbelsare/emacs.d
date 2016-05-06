d9995268dac076b97fd081b2d8884093e4a76451
Glynn Forrest <me@glynnforrest.com>
2016-05-06 19:22:28 +0100

    Yaml mode

--- .spacemacs
+++ .spacemacs
@@ -30,6 +30,7 @@ values."
      ;; markdown
      org
      php
+     yaml
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
--- gf-general/packages.el
+++ gf-general/packages.el
@@ -3,8 +3,13 @@
     wgrep-helm
     ;; remove after https://github.com/syl20bnr/spacemacs/pull/5831 is in a release
     request
+    yaml-mode
     ))
 
 (defun gf-general/init-wgrep-helm ()
   (use-package wgrep-helm
     ))
+
+(defun gf-general/post-init-yaml-mode ()
+  (add-hook 'yaml-mode-hook (lambda ()
+                              (setq yaml-indent-offset 4))))
