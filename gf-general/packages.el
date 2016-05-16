0b9df843e856c3b39dd6e0958ef24a3392459f6d
Glynn Forrest <me@glynnforrest.com>
2016-05-16 11:26:44 +0100

    Add rainbow mode to scss

--- gf-general/packages.el
+++ gf-general/packages.el
@@ -1,9 +1,10 @@
 (defconst gf-general-packages
   '(
     wgrep-helm
+    rainbow-mode
     ;; remove after https://github.com/syl20bnr/spacemacs/pull/5831 is in a release
     request
-    yaml-mode
+    scss-mode
     ))
 
 (defun gf-general/init-wgrep-helm ()
@@ -13,3 +14,8 @@
 (defun gf-general/post-init-yaml-mode ()
   (add-hook 'yaml-mode-hook (lambda ()
                               (setq yaml-indent-offset 4))))
+
+(defun gf-general/post-init-scss-mode ()
+  (setq css-indent-offset 2)
+  (add-hook 'scss-mode-hook (lambda ()
+                              (rainbow-mode t))))
