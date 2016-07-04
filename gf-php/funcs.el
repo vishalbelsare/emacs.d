129bf63b0dc265ae1ae3024fb0800b9763ce942d
Glynn Forrest <me@glynnforrest.com>
2016-07-04 14:10:41 +0100

    Improve gf-php/current-file-namespace with a script

--- gf-php/funcs.el
+++ gf-php/funcs.el
@@ -1,23 +1,11 @@
 (defun gf-php/current-file-namespace ()
   "Get a suitable namespace for the current file."
-  (interactive)
-  (if (s-contains? "src" (buffer-file-name))
-      (gf-php/namespace-from-path buffer-file-name "src")
+  (gf-php/file-namespace buffer-file-name))
 
-    (if (s-contains? "tests" (buffer-file-name))
-        (gf-php/namespace-from-path buffer-file-name "tests")
-
-      (if (s-contains? "app" (buffer-file-name))
-          (gf-php/namespace-from-path buffer-file-name "app")))))
-
-(defun gf-php/namespace-from-path (path substr)
-  "Extract a namespace from a path name that contains `substr`."
-  (let ((namespace-file
-         (s-right (- (length path)
-                     (s-index-of substr path)
-                     (+ 1 (length substr)))
-                  path)))
-    (s-join "\\" (butlast (s-split "/" namespace-file)))))
+(defun gf-php/file-namespace (file)
+  "Get a suitable namespace for `FILE`."
+  (shell-command-to-string
+   (concat "~/.spacemacs-layers/bin/php_resolve_namespace.php " file)))
 
 (defun gf-php/go-to-namespace ()
   "Go to the namespace declaration at the top of this file."
